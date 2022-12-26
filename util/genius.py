77e9b6b3fdcc5f15292a03e11d4ce3b2bc3df26d
Rui Nogueira <rui.nogueira@sap.com>
2022-12-26 22:22:49 +0100

    update

--- util/genius.py
+++ util/genius.py
@@ -7,6 +7,8 @@ from util.metadata import hasArtist, hasLyrics, hasTitle
 
 GENIUS_ACCESS_TOKEN = os.getenv('GENIUS_ACCESS_TOKEN')
 
+print("GENIUS_ACCESS_TOKEN = >" + GENIUS_ACCESS_TOKEN + "<")
+
 # if GENIUS_ACCESS_TOKEN and len(GENIUS_ACCESS_TOKEN) > 0:
 
 genius = Genius(GENIUS_ACCESS_TOKEN)
